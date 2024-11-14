e7df23b11cbf8746767e20b1c9e204859d4f000e
SukanyaT2022 <sukanyat2022a@gmail.com>
2024-11-14 09:59:49 -0500

    add

--- index.js
+++ index.js
@@ -1,72 +1,15 @@
-var storePlusorMinus = ''// store the sign plus or minue
-var storeEasyMediumHard =' '//store user pick what level
-var num1 = 0
-var num2 = 0
-
-
-function startShowBtnFunc(){
-   document.getElementById('addSubtractWrapper').style.display = 'block'
-}
-
-function plusFunc(){
-    // when click easy medium hard section show
- document.getElementById('easyHardWrapper').style.display = 'block';
- storePlusorMinus = "+"
-}
-
-function minusFunc(){
-    // when click easy medium hard section show
- document.getElementById('easyHardWrapper').style.display = 'block';
- storePlusorMinus = "-"
-}
-
-function easyFunc(){
-    // when click radio easy questionanswerwrapper open
-    document.getElementById('questionAnswerWrapper').style.display = 'block';
-    storeEasyMediumHard ='easy' //easy is value on html
-    generateQuestion()
-}
-
-function mediumFunc(){
-    // when click radio easy questionanswerwrapper open
-    document.getElementById('questionAnswerWrapper').style.display = 'block';
-    storeEasyMediumHard ='medium' //meduim is value on html
-    generateQuestion()
-}
-
-function hardFunc(){
-    // when click radio easy questionanswerwrapper open
-    document.getElementById('questionAnswerWrapper').style.display = 'block';
-    storeEasyMediumHard ='hard' //hard is value on html
-    generateQuestion()
-
-}
-
-// this function generate random number after clikc - or minus and level
-function generateQuestion(){
-if(storeEasyMediumHard=="easy"){
-    // generate random number below bet 1 to 10
-    num1 = Math.floor(Math.random() * 10) + 1;
-    num2 = Math.floor(Math.random() * 10) + 1;
-}else if(storeEasyMediumHard=="medium"){
-   // generate random number below bet 11 to 100
-   num1 = Math.floor(Math.random() * (100 - 11 + 1)) + 11;
-   num2 = Math.floor(Math.random() * (100 - 11 + 1)) + 11;
-}else{
-     // generate random number below bet 111 to 1000
-
-     num1 = Math.floor(Math.random() * (1000 - 111 + 1)) + 111;
-     num2 = Math.floor(Math.random() * (1000 - 111 + 1)) + 111; 
-}
-document.getElementById("questionPtag").innerText = num1 + " " + storePlusorMinus + " " + num2
-
 var storePlusorMinus = ""; // store the sign plus or minue
 var storeEasyMediumHard = " "; //store user pick what level
 var num1 = 0;
 var num2 = 0;
+var score = 0;
+var questionNum = 0;
 
 function startShowBtnFunc() {
   document.getElementById("addSubtractWrapper").style.display = "block";
+  questionNum = questionNum + 1;
+  document.getElementById("questionNumID").innerText =
+    "Question No." + questionNum;
 }
 
 function plusFunc() {
@@ -84,15 +27,14 @@ function minusFunc() {
 function easyFunc() {
   // when click radio easy questionanswerwrapper open
   document.getElementById("questionAnswerWrapper").style.display = "block";
-  document.getElementById("resetButtonID").style.display = "block";
   storeEasyMediumHard = "easy"; //easy is value on html
   generateQuestion();
+  document.getElementById("resetButtonID").style.display = "block";
 }
 
 function mediumFunc() {
   // when click radio easy questionanswerwrapper open
   document.getElementById("questionAnswerWrapper").style.display = "block";
-  document.getElementById("resetButtonID").style.display = "block";
   storeEasyMediumHard = "medium"; //meduim is value on html
   generateQuestion();
 }
@@ -100,7 +42,6 @@ function mediumFunc() {
 function hardFunc() {
   // when click radio easy questionanswerwrapper open
   document.getElementById("questionAnswerWrapper").style.display = "block";
-  document.getElementById("resetButtonID").style.display = "block";
   storeEasyMediumHard = "hard"; //hard is value on html
   generateQuestion();
 }
@@ -124,41 +65,23 @@ function generateQuestion() {
   document.getElementById("questionPtag").innerText =
     num1 + " " + storePlusorMinus + " " + num2;
 }
-// formula for random
-// num1 = Math.floor(Math.random() * (max - min + 1)) + min;
-
-// this function check if the answer is correct or Not when click submitbtn
-function checkAnswer(){
-    // read id or answer from user
-var answer = parseInt(document.getElementById("answerInputBox").value)
-var ans = 0 
-
-// answer from formula
-if (storePlusorMinus=="+"){
-ans = num1 + num2
-}else if(storePlusorMinus=="-"){
-    ans = num1 - num2
-}
-
-if (ans == answer){
-    alert('Correct Answer!')
-}else{
-    alert('Not Correct Answer :(')
-}
-}
-
+// submit button
 function checkAnswer() {
   // read id or answer from user
-  var answer = parseInt(document.getElementById("answerInputBox").value);
+  var answer = (document.getElementById("answerInputBox").value);
+  if (answer == ""){
+    alert('Please enter the answer!')
+  }else{
+    answer = parseInt(answer)
   var ans = 0;
-
+  document.getElementById("answerInputBox").value = " ";
   // answer from formula
   if (storePlusorMinus == "+") {
     ans = num1 + num2;
   } else if (storePlusorMinus == "-") {
     ans = num1 - num2;
   }
-
+document.getElementById('nextBtn').style.visibility = "visible";
   if (ans == answer) {
     document.getElementById("showImgCorrectNotcorrect").src =
       "/asset/image/correct2.png";
@@ -166,6 +89,7 @@ function checkAnswer() {
     document.getElementById("showImgCorrectNotcorrect").src =
       "/asset/image/notcorrect.png";
   }
+}
 }
 
 function resetFunc() {
@@ -177,8 +101,17 @@ function resetFunc() {
   var storeEasyMediumHard = " "; //store user pick what level
   var num1 = 0;
   var num2 = 0;
-  document.getElementById("answerInputBox").value = " "
-  document.getElementById("questionPtag").innerText = ""
-  document.getElementById("showImgCorrectNotcorrect").src = ""
+  document.getElementById("answerInputBox").value = " ";
+  document.getElementById("questionPtag").innerText = "";
+  document.getElementById("showImgCorrectNotcorrect").src = "";
+}
+function nextQuestionFunc() {
+  questionNum = questionNum + 1;
+  document.getElementById("questionNumID").innerText =
+    "Question No." + questionNum;
+    generateQuestion()
+    document.getElementById('nextBtn').style.visibility = "hidden";
+    document.getElementById("showImgCorrectNotcorrect").src =
+      "";
 
-}}
+}
